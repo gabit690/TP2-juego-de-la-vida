ea13bec899f40d07f9073019bdbc425f33b4803f
santicouce <santicouce@outlook.com>
2018-10-31 12:21:06 -0300

    Add files via upload

---
+++ celula.cpp
@@ -0,0 +1,36 @@
+
+struct Celula{
+private:
+	Energia energia;
+	bool estado;
+	Color color;
+public:
+	Celula(bool estadoInicial,Energia energiaInicial,Color colorInicial){
+		energia=energiaInicial;
+		estado=estadoInicial;
+		color=colorInicial;
+	}
+	void cambiarEstadoDeLaCelula(){
+		if (estado){
+			estado=false;
+		}else{
+			estado=true;
+		}
+	}
+	void cambiarColorDeLaCelula(Color nuevoColor){
+		color=nuevoColor;
+	}
+	void cambiarEnergia(Energia nuevaEnergia){
+		energia=nuevaEnergia;
+	}
+	Celula crearCopiaDeLaCelula(Celula celulaACopiar){
+		Celula nuevaCopia;
+		nuevaCopia->color=celulaACopiar->color;
+		nuevaCopia->energia=ceulaACopiar->energia;
+		nuevaCopia->estado=ceulaACopiar->estado;
+		return nuevaCopia;
+	}
+	~Celula(){
+
+	}
+};
